84242d53b101936675bc0239ec9396d0a04b1306
callum fisher <notcallumfisher@outlook.com>
2025-12-03 21:28:06 +0000

    v1.0.11
    Update metadata and version in rewritten-2024.js

--- 2015/v1/rewritten-2024.js
+++ 2015/v1/rewritten-2024.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         EpicOS (v1) - Rewritten
-// @namespace    https://github.com/cffisher/EpicOS
-// @version      1.0.9
+// @namespace    callum fisher
+// @version      1.0.11
 // @license      Unlicense
 // @description  A rewrite of the original 2015 web browser version of EpicOS for Multiplayer Piano.
-// @author       Callum Fisher <fishercallum@proton.me>
+// @author       callum fisher
 // @match        *://mppclone.com/*
 // @match        *://mpp.8448.space/*
 // @match        *://multiplayerpiano.com/*
@@ -23,45 +23,13 @@
 // ==/UserScript==
 
 /*
-	"EpicOS (v1) - Rewritten"
-	2024.12.21 - 2025.01.10
+	callum fisher - 2024.12.21 - 2025.07.31
 
 	This is a rewrite of the original 2015 web browser version of EpicOS. I have:
 	- Compiled separate message listeners into one
 	- Changed the command handler to a switch statement
 	- Removed unnecessary type coercion
 	- Added an anti-spam chat send function
-
-	Userscript: https://greasyfork.org/scripts/521353
-	
-	Repository: https://github.com/cffisher/EpicOS
-
-	Callum Fisher <fishercallum@proton.me>
-
-	This is free and unencumbered software released into the public domain.
-
-	Anyone is free to copy, modify, publish, use, compile, sell, or
-	distribute this software, either in source code form or as a compiled
-	binary, for any purpose, commercial or non-commercial, and by any
-	means.
-
-	In jurisdictions that recognize copyright laws, the author or authors
-	of this software dedicate any and all copyright interest in the
-	software to the public domain. We make this dedication for the benefit
-	of the public at large and to the detriment of our heirs and
-	successors. We intend this dedication to be an overt act of
-	relinquishment in perpetuity of all present and future rights to this
-	software under copyright law.
-
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-	IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-	OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-	OTHER DEALINGS IN THE SOFTWARE.
-
-	For more information, please refer to <https://unlicense.org/>
 */
 
 const startEOS = () => {
@@ -77,8 +45,8 @@ const startEOS = () => {
 
 	let selfID = MPP.client.getOwnParticipant()._id; // Your _ID on Multiplayer Piano
 
-	let version = '1.0.9';
-	let editDate = '2025.01.10';
+	let version = '1.0.10';
+	let editDate = '2025.07.31';
 
 	console.log(`[EpicOS (v1) - Rewritten v${version}] Running.`);
 
