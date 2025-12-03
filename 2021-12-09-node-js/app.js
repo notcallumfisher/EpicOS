e9cf4e9ba0474c75322507696a24c21d209b6ffc
callum fisher <notcallumfisher@outlook.com>
2025-12-03 20:16:00 +0000

    Clean up app.js by removing comments
    Removed extensive comments and licensing information from app.js.

--- 2021-12-09-node-js/app.js
+++ 2021-12-09-node-js/app.js
@@ -1,43 +1,4 @@
-/*
-	"EpicOS 2021"
-	app.js - Main program.
-	2021.12.09
-
-	This is a Node.js version of EpicOS from 2021 published for archival purposes.
-	It supports multiple instances and features a MIDI player. Most of this code was replaced or updated by 2023 in a new version.
-	It also features a attempts at bot identification and afk detection from before MultiplayerPiano.net added tags to bots and tags to afk users.
-	You might notice that the related messages are commented out as they became pretty annoying.
-	The main app.js was archived & replaced while commands continued to be used & updated, so this is the only archived file.
-
-	Repository: https://github.com/cffisher/EpicOS
-
-	Callum Fisher <fishercallum@proton.me>
-
-	This is free and unencumbered software released into the public domain.
-
-	Anyone is free to copy, modify, publish, use, compile, sell, or
-	distribute this software, either in source code form or as a compiled
-	binary, for any purpose, commercial or non-commercial, and by any
-	means.
-
-	In jurisdictions that recognize copyright laws, the author or authors
-	of this software dedicate any and all copyright interest in the
-	software to the public domain. We make this dedication for the benefit
-	of the public at large and to the detriment of our heirs and
-	successors. We intend this dedication to be an overt act of
-	relinquishment in perpetuity of all present and future rights to this
-	software under copyright law.
-
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-	IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-	OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-	OTHER DEALINGS IN THE SOFTWARE.
-
-	For more information, please refer to <https://unlicense.org/>
-*/
+// callum fisher - 2021.12.09
 
 // Dependencies ++
 const fs = require("fs");
