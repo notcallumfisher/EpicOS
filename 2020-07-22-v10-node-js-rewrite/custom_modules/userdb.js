3181fe82323ee81f8d2e5d83df37f2021e430576
callum fisher <notcallumfisher@outlook.com>
2026-01-03 19:37:40 +0000

    Update userdb.js

--- 2020-07-22-v10-node-js-rewrite/custom_modules/userdb.js
+++ 2020-07-22-v10-node-js-rewrite/custom_modules/userdb.js
@@ -1,11 +1,6 @@
 /*
-	"EpicOS (v10) - Node.js Rewrite"
+	callum fisher - 2020.07.22
 	userdb.js - For handling user data.
-	2020.07.22
-
-	Repository: https://github.com/cffisher/EpicOS
-
-	Callum Fisher <fishercallum@proton.me>
 
 	This is free and unencumbered software released into the public domain.
 
